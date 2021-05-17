0a14cef5ebe9dbb6ec6995a93e4a331769027991
marcluettecke <marc.luettecke1@gmail.com>
2021-05-17 14:17:21 +0200

    introduced project carousel component and updated tech stack

---
+++ src/components/Projects.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Carousel } from '3d-react-carousal';
+import SingleImage from './SingleImage';
+import content from '../content';
+
+let slides = content.projects.websites.map(website => {
+	return <SingleImage key={website.name} website={website} />;
+});
+
+const Projects = props => {
+	return (
+		<div className='min-h-screen font-dosis text-center'>
+			<h1 className='text-5xl font-bold mt-20'>My past projects</h1>
+			<div id='projects' className='mt-10'>
+				<Carousel slides={slides} autoplay={true} interval={5000} />
+			</div>
+		</div>
+	);
+};
+export default Projects;
